e34b34d7c45757296181f2ce28bea3fcdb809a75
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-05-20 00:09:22 -0300

    modifique views

--- home/views.py
+++ home/views.py
@@ -14,6 +14,9 @@ def resumen(request):
 def proyecto_F4(request):
     return render(request, "home/proyecto_F4.html")
 
+def noticias(request):
+    return render(request, "home/noticias.html")
+
 def galeria(request):
     return render(request, "home/galeria.html")
 
@@ -28,3 +31,18 @@ def contacto(request):
         contact_form = UserInfoForm()
 
     return render(request, "home/contacto.html", {'contact_form': contact_form})
+
+
+def new_notice(request):
+    if request.method == 'POST':
+        contact_form = UserInfoForm(request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.success(request, 'Mensaje enviado 
+            correctamente.')
+            return redirect('home/confirmacion_envio.html')
+        else:
+            contact_form = UserInfoForm()
+
+    return render(request, "home/contacto.html", {'contact_form': 
+    contact_form})
