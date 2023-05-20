55537853b32b2bf26add4448a91259204dbaa48d
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-05-19 23:53:35 -0300

    puse la url de noticias

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('home/sobre_mi/', views.sobre_mi, name="sobre_mi"),
     path('home/resumen/', views.resumen, name="resumen"),
     path('home/proyecto_F4/', views.proyecto_F4, name="proyecto_F4"),
+    path('home/noticias/', views.noticias, name="noticias"),
     path('home/galeria/', views.galeria, name="galeria"),
     path('home/contacto/', views.contacto, name="contacto"),
 ]
